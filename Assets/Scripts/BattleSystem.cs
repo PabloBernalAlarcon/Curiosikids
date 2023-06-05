62ef3a0a81ae6d0b2bdfd1046c8e8ba6344ede1d
Pablo Bernal Alarcón <bononov6@yahoo.com>
2023-06-05 15:12:33 -0500

    Added HUD for battle info
    -Battle info HUD
    -Adjusted prefabs
    -Added UI elements for text

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WIN, LOSE}
 public class BattleSystem : MonoBehaviour
 {
+
+    public TMP_Text dialogueText;
 
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
 
     public Transform playerBattleStation;
     public Transform enemyBattleStation;
+
+    private Unit playerUnit;
+    private Unit enemyUnit;
 
     public BattleState bState;    // Start is called before the first frame update
     void Start()
@@ -21,9 +27,11 @@ public class BattleSystem : MonoBehaviour
 
     void SetupBattle()
     {
-        Instantiate(playerPrefab,playerBattleStation);
-        Instantiate(enemyPrefab,enemyBattleStation);
-
+       GameObject pleyer = Instantiate(playerPrefab,playerBattleStation);
+        playerUnit = pleyer.GetComponent<Unit>();
+        GameObject enemi = Instantiate(enemyPrefab,enemyBattleStation);
+        enemyUnit = enemi.GetComponent<Unit>();
+        dialogueText.text = "A wild "+enemyUnit.unitName+" has appeared!";
     }
 
 }
